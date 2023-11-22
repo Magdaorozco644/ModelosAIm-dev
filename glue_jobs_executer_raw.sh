1034fd3556032c0b14c8e901203d3029024d2ab5
sdlf-deployment-user-at-283731589572 <ian.saura@strata-analytics.us>
2023-11-22 14:58:45 -0300

    add buildspec for codebuild and the executer

---
+++ glue_jobs_executer_raw.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Archivo JSON con la configuración de los trabajos
+JOBS_FILE="glue-jobs\raw\bulk\jobs-config\jobs.json"
+
+# Leer cada trabajo del archivo JSON y crearlo
+cat "$JOBS_FILE" | python -c '
+import json
+import sys
+import subprocess
+
+# Leer el JSON desde la entrada estándar
+jobs = json.load(sys.stdin)
+
+# Iterar sobre cada trabajo y ejecutar el comando aws glue create-job
+for job in jobs:
+
+    json_str = json.dumps(job)
+    subprocess.run(["aws", "glue", "create-job", "--cli-input-json", json_str])
+
+    # Iniciar el trabajo
+    start_job_cmd = ["aws", "glue", "start-job-run", "--job-name", job["Name"]]
+    subprocess.run(start_job_cmd)
+'
